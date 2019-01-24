1020a5c9e5cbb6fa7055854429f7afc47d1831c6
NataliaBach94 <46976790+NataliaBach94@users.noreply.github.com>
2019-01-24 19:08:22 +0100

    Create Main.java

---
+++ src/pl/NataliaBach94/UdemyJava/Main.java
@@ -0,0 +1,11 @@
+package pl.NataliaBach94.UdemyJava;
+
+// to jest komentarz który nic nie zmieni
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello, World");
+        /* to jest komentarz na kilka lini
+        np. dwie lub trzy
+         */
+    }
+}
