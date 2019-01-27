206982c0a96637407fda796f59a76e1baee19ccc
NataliaBach94 <46976790+NataliaBach94@users.noreply.github.com>
2019-01-27 21:59:17 +0100

    args

--- src/pl/NataliaBach94/UdemyJava/Main.java
+++ src/pl/NataliaBach94/UdemyJava/Main.java
@@ -3,9 +3,10 @@ package pl.NataliaBach94.UdemyJava;
 // to jest komentarz który nic nie zmieni
 public class Main {
     public static void main(String[] args){
-        System.out.println("Hello, World");
+        System.out.println("Hello " + args[0]);
         /* to jest komentarz na kilka lini
-        np. dwie lub trzy
+        np.dwie lub trzy
          */
+
     }
 }
